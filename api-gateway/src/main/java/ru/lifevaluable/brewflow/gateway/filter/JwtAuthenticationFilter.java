791be42f971b44af655aa79920a9fd07979a2c8e
Nikita Kovrigin <kovrnikd@yandex.ru>
2025-10-02 15:24:07 +0300

    [api-gateway] мелкие правки логирования и настроек

--- api-gateway/src/main/java/ru/lifevaluable/brewflow/gateway/filter/JwtAuthenticationFilter.java
+++ api-gateway/src/main/java/ru/lifevaluable/brewflow/gateway/filter/JwtAuthenticationFilter.java
@@ -25,10 +25,9 @@ public class JwtAuthenticationFilter implements GlobalFilter, Ordered {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-
         ServerHttpRequest request = exchange.getRequest();
         String path = request.getURI().getPath();
-        log.debug("Got request {}", request.toString());
+        log.debug("Got request {}", path);
         if (isPublicPath(path)) {
             return chain.filter(exchange);
         }
