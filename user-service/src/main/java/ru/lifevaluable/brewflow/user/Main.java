3beef73f283b5f7ba4052cb4f2017677040a0dfb
Nikita Kovrigin <kovrnikd@yandex.ru>
2025-10-21 12:55:47 +0300

    [user-service,order-service] убраны warning

--- user-service/src/main/java/ru/lifevaluable/brewflow/user/Main.java
+++ user-service/src/main/java/ru/lifevaluable/brewflow/user/Main.java
@@ -2,9 +2,12 @@ package ru.lifevaluable.brewflow.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    UserDetailsServiceAutoConfiguration.class
+})
 @EnableJpaRepositories
 public class Main {
     public static void main(String[] args) {
