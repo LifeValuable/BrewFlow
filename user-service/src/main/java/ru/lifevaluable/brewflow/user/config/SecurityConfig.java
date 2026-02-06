8fc19472be7bca7632daafbfa9f8e60bee37d3f6
Nikita Kovrigin <kovrnikd@yandex.ru>
2026-02-06 13:44:16 +0300

    [user-service] закрыт контроллер internal, пока не реализована до конца логика

--- user-service/src/main/java/ru/lifevaluable/brewflow/user/config/SecurityConfig.java
+++ user-service/src/main/java/ru/lifevaluable/brewflow/user/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
                     .requestMatchers("/auth/**").permitAll()
                     .requestMatchers("/actuator/**").permitAll()
                     .requestMatchers("/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**").permitAll()
-                    .requestMatchers("/internal/**").permitAll()
+                    .requestMatchers("/internal/**").denyAll()
                     .anyRequest().authenticated()
                 )
                 .exceptionHandling(ex -> ex
